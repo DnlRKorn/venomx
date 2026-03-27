bdfe67823f4e7f3dfc826ee437acf48fc59b5e5d
DnlRKorn <6885702+DnlRKorn@users.noreply.github.com>
2026-03-27 13:52:35 +0000

    Deploying to gh-pages from @ DnlRKorn/venomx@96d224adba778452161d760cbbaf2cee1bcdd49e 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.2.0.post2.dev0+3ebcd9f',
+    VERSION: '0.2.0.post3.dev0+96d224a',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
